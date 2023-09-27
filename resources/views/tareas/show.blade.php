39a58ee0ebc33ede09fddbf3ef59ba5898a9b92c
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-09-27 09:48:27 -0300

    Se edita las rutas correctamente (eliminar y modificar)

--- resources/views/tareas/show.blade.php
+++ resources/views/tareas/show.blade.php
@@ -6,15 +6,17 @@ home se utiliza como plantilla base  --}}
 @section('content')
     <div class="container w5 border p-4 mt-4">
         {{-- se llama a la ruta home dentro del formulario y se utiliza el metodo post para enviar --}}
-        <form action="{{ route('home-show', ['id' => $tarea->id]) }}" method="POST">
+        <form action="{{ route('home-update', ['id' => $tarea->id]) }}" method="POST">
+            @method('PATCH')
             {{-- cSrf = cross site request forgery. sirve para autenticar (token) --}}
             @csrf
             @if (session('success'))
                 <h6 class="alert alert-success">{{ session('success') }}</h6>
             @endif
             @error('titulo')
-                <h6 class="alert alert-danger">{{ session($message) }}</h6>
+                <h6 class="alert alert-danger">{{ $message }}</h6>
             @enderror
+
             <div class="mb-3">
                 <label for="title" class="form-label">Titulo de la tarea</label>
                 <input type="text" name="titulo" class="form-control" value="{{ $tarea->titulo }}">
