e28237b9ecd1bcd8dd6fec4b9b53d4bd9d43c207
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-09-27 09:45:43 -0300

    Update TareasController.php

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -10,27 +10,29 @@ class TareasController extends Controller
     /**index para mostrar todas las tareas
     store para guardar una tarea
     update para actualizar una tarea
-    destroy para eliminar
-    edit para editar */
+    destroy para eliminar*/
+
+    public function index()
+    {
+        $tarea = Tarea::all();
+        return view('tareas.tareas', ['tarea' => $tarea]);
+    }               //carpeta.vista
 
     public function store(Request $request)
     {
-        // Validamos lo que recibimos, en este caso es solo requerido
+        // Validamos lo que recibimos, en este caso es requerido y un minimo de cuatro letras
         $request->validate([
-            'titulo' => 'required'
+            'titulo' => 'required|min:4'
+
         ]);
         //creamos un nuevo objeto tarea y asignamos los valores ingresados a la columna
         $tarea = new Tarea;
         $tarea->titulo = $request->titulo;
         $tarea->save();
-        //redireccionamos y damos aviso que de secess
-        return redirect()->route('home')->with('success', 'Tarea creada correctamente');
+        //redireccionamos y damos aviso que de success 
+        return redirect()->route('home')->with('success', 'Tarea creada con exito');
     }
-    public function index()
-    {
-        $tarea = Tarea::all();
-        return view('tareas.tareas', ['tarea' => $tarea]);
-    }
+
 
     public function show($id)
     {
@@ -38,14 +40,18 @@ class TareasController extends Controller
         return view('tareas.show', ['tarea' => $tarea]);
     }
 
-    public function update()
+    public function update(Request $request, $id)//modifica la tarea
     {
-        $tarea = Tarea::all();
-        return view('tareas.tareas', ['tarea' => $tarea]);
+        $tarea = Tarea::find($id);
+        $tarea->titulo = $request->titulo;
+        $tarea->save();
+
+        return redirect()->route('home')->with('success', 'Tarea actualizada con exito');
     }
-    public function destroy()
+    public function destroy($id) //function para eliminar una tarea
     {
-        $tarea = Tarea::all();
-        return view('tareas.tareas', ['tarea' => $tarea]);
+        $tarea = Tarea::find($id);
+        $tarea->delete();
+        return redirect()->route('home')->with('success', 'Tarea Eliminada con exito');
     }
 }
