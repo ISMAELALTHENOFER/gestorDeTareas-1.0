b71400d31f0296d68a8ce8d89ca4566dfc2b1530
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-10-17 10:19:47 -0300

    Se realiza la relacion HasMany hacia tarea
    Una categoria puede tener muchas tareas

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use app\Models\Tarea;
+
 
 class Categoria extends Model
 {
     use HasFactory;
+
+    public function tareas()
+    {
+        return $this->hasMany(Tarea::class); //relacion uno a muchos (una categoria puede tener muchas tareas)
+    }
 }
