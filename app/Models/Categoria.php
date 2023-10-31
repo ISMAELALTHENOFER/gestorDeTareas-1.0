2b6a5fe2882a71fa9035156712391d6549635ba0
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-10-31 07:00:25 -0300

    Update Categoria.php

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use app\Models\Tarea;
+use App\Models\Tarea;
 
 
 class Categoria extends Model
